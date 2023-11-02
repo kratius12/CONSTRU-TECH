6cc3fa7a811509531900047f2aa3c40ef9022ecd
pilaruwu <131707990+pilaruwu@users.noreply.github.com>
2023-11-02 15:51:05 -0500

    Arreglo tabla

--- client/src/components/ClienteTable.jsx
+++ client/src/components/ClienteTable.jsx
@@ -54,7 +54,6 @@ export default function ClienteTable({clientes}) {
                       onChange={() => handleClick(idCli, estado)}
                     />
                   </div>
-                  {estado}
                 </td>
                 <td>
                   <a
