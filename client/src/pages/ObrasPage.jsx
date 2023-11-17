f7b0578da65815673e35e90ee17c36c3665bf6c9
pilaruwu <131707990+pilaruwu@users.noreply.github.com>
2023-11-16 22:44:02 -0500

    Nuevos cambios

--- client/src/pages/ObrasPage.jsx
+++ client/src/pages/ObrasPage.jsx
@@ -20,12 +20,26 @@ function ObrasPage() {
 
     return(
         <div>
+<<<<<<< Updated upstream
             <h1 className="text5-xl text-white font-bold text-center">Obras</h1>
                 <button className="block my-3 rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={ ()=> navigate(`/formTemplate`)}>
                     Agregar obra
                 </button>
             <div className="grid grid-cols-3 gap-2">
                 {renderMain()}
+=======
+            <h1 className=" font-bold text-left">Obras</h1>
+
+            <div className="table-responsive">
+                <div className="row">
+                    <div className="col-md-6 mb-3">
+                        <button className="btn btn-primary" onClick={ ()=> navigate(`/agregarObra`)}>
+                            Agregar obra
+                        </button>                      
+                    </div>
+                    {renderMain()}
+                </div>
+>>>>>>> Stashed changes
             </div>
 
         </div>
---
+++ client/src/pages/RolesPage.jsx
@@ -0,0 +1,65 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import RolTable  from "../components/RolesTable"
+import { useRol } from "../context/RolesProvider";
+import TableInfo from "../components/TableInfo";
+
+function RolesPage() {
+
+    const dataHeader = [
+        {
+           header: "ID",
+           accessorKey: 'idRol'
+
+        },
+        {
+            header: "Nombre",
+            accessorKey: 'nombre'
+        },
+        {
+            header: "Estado",
+            accessorKey: 'estado',
+            idProperty: 'idRol'
+        },
+        {
+            header: "Accion",
+            accessorKey: 'accion',
+            idProperty: 'idRol'
+        }
+    ]
+
+    const {roles, Roles} = useRol()
+    const navigate = useNavigate()
+    useEffect(() =>{
+    Roles()  
+    }, [])
+
+    function renderMain() {
+        if (roles.length === 0) {
+            return <h1>Sin Roles</h1>
+            
+        }else{
+            return <TableInfo dataHeader={dataHeader} dataBody={roles} routeEdit={'editarRol'} viewDetail/>
+        }
+        // return <RolTable roles={roles}/>
+    }
+
+    return(
+        <div>
+            <h1 className="text5-xl text-black font-bold text-left my-3">Roles</h1>
+            <div className="table-responsive">
+                <div className="row">
+                    <div className="col-md-6 mb-3">
+                        <button className="btn btn-primary" onClick={ ()=> navigate(`/agregarRol`)}>
+                            Agregar rol
+                        </button>                        
+                    </div>
+                    {renderMain()}
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default RolesPage
