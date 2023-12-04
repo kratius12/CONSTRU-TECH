b1d98995e0026e9ae32e660f9a1e3e99443300c1
kevincastrillon70 <kevincastrillon31@gmail.com>
2023-12-04 09:09:15 -0500

    Se agrego ventana de detalle
    Se agregaron propiedades a al componente TableInfo y AlertDetail
    Se agrego campo para obtener las especialidades del empleado

--- client/src/components/AlertDetail.jsx
+++ client/src/components/AlertDetail.jsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import { useEffect, useState, useId } from 'react';
+import {renderToString} from 'react-dom/server'
+function AlertDetail({ id, entity, getApi }) {
+  const [infoDetail, setInfoDetail] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getApi(id);
+        setInfoDetail(data);
+        setTextStatus(data.estado)
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
+    fetchData();
+  }, [getApi, id]);
 
-function AlertDetail({dataHeader, dataBody, entity}) {
+  const excludedFields = ['createdAt', 'updatedAt', 'empleado_especialidad']
+  const [textStatus, setTextStatus] = useState(1)
+  const text = textStatus == 1 ? 'Activo' : 'Inactivo'
+  const tableDetail = () => {
+    if (!infoDetail) {
+      return null; 
+    }
+    if (infoDetail.empleado_especialidad && infoDetail.empleado_especialidad.length == 0) {
+      excludedFields.push('empleado_especialidad')
+    }
+    return (
+      <table className="w-100">
+        <tbody>
+          {Object.entries(infoDetail).filter(([key]) => !excludedFields.includes(key)).map(([key, value]) => (
+            
+            <tr key={key}>
+              <th>{key}</th>
+              <td>{key == 'estado' ? text : value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
 
- const tableDetail = () =>{
-  let tableContent = '<table class="w-100">'
-    dataHeader.forEach((item)=> {
-      tableContent += `<tr><th>${item.header}</th><td>${dataBody[item]}</td></tr>`
-    })
-  tableContent += '</table>';
-
-  return tableContent;
- }
-  
   const alertConfirm = () => {
-    const content = tableDetail()
-
+    const content = tableDetail();
     $.confirm({
-      title: 'Detalle '+entity,
-      content: content,
+      title: 'Detalle ' + entity,
+      content: renderToString(content),
       icon: 'fa fa-info-circle',
       theme: 'modern',
       closeIcon: true,
@@ -35,9 +62,9 @@ function AlertDetail({dataHeader, dataBody, entity}) {
 
   return (
     <>
-    <button onClick={() => alertConfirm()} className="btn bg-secondary text-white mx-3">
-      Ver <i className="fa-solid fa-eye"></i>
-    </button>    
+      <button onClick={() => alertConfirm()} className="btn bg-secondary text-white mx-3">
+        Ver <i className="fa-solid fa-eye"></i>
+      </button>
     </>
   );
 }
