5642518b9d8872eaa2f53ebc17f98ccc9f36666c
kratius12 <91803197+kratius12@users.noreply.github.com>
2024-02-06 10:45:45 -0500

    Cambios en obras

--- client/src/pages/obras/ValidateObra.jsx
+++ client/src/pages/obras/ValidateObra.jsx
@@ -14,14 +14,18 @@ export const obraSchemaEdit = Yup.object().shape({
     idCliente: Yup.string().required("Seleccione el cliente"),
     idEmp: Yup.string().required("El empleado es requerido"),
     area: Yup.string().required("El area es requerida"),
-    fechaini: Yup.date().required("La fecha de incio de la obra es requerida"),
-    fechafin: Yup.date().required("La fecha de fin de la obra es requerida"),
+    fechaini: Yup.date()
+        .min(new Date(), 'La fecha de inicio debe ser posterior a la actual')
+        .required("La fecha de inicio de la obra es requerida"),
+    fechafin: Yup.date()
+        .min(Yup.ref('fechaini'), 'La fecha de fin debe ser posterior a la fecha de inicio')
+        .required("La fecha de fin de la obra es requerida"),
     precio: Yup.string().required("El precio de la obra es requerido"),
     descripcion: Yup.string().required("La descripción de obra de requerida"),
     estado: Yup.string().required("El estado es requerido"),
-})
+});
 
-export const actividades = Yup.object().shape({
+export const actividad = Yup.object().shape({
         descripcion: Yup.string().required("La descripción de la actividad es requerida"),
         fechaini: Yup.date().required("La fecha de inicio de la actividad es requerida"),
         fechafin: Yup.date().required("La fecha de fin de la actividad es requerida"),
