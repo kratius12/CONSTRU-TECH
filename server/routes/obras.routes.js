26659aa5e1f326672757336dd0af0bd4eaa77e07
elsiri <30646221+elsiri@users.noreply.github.com>
2024-02-19 16:21:42 -0500

    Se corrigio error en las rutas

--- server/routes/obras.routes.js
+++ server/routes/obras.routes.js
@@ -2,7 +2,7 @@ import { Router, json } from "express";
 
 
 import { PrismaClient } from "@prisma/client";
-import { ucfirst } from "../plugins";
+import { ucfirst } from "../plugins.js";
 
 const prisma = new PrismaClient()
 const router = Router()
